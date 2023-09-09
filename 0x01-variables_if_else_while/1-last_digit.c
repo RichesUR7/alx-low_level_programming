c830ce9ea93a04618c77484860fa070759cf60f8
RichesUR7 <richesuche91@gmail.com>
2023-09-09 04:14:54 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int n;
-
-	int m;
+	int lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	if (m > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	if (lastd == 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	else if (m < 6 && m != 0)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
 	else
-	printf("Last digit of %s is %d and is 0\n," n, m);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n," n, lastd);
 	return (0);
+
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print alphabet except q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
 
+	while (alphabet <= 'z')
+	if (alphabet != 'q' && alphabet != 'e')
+
+	putchar(alphabet);
+
+	alphabet++;
+
+	putchar('\n');
+	return (0);
+
+
+}
