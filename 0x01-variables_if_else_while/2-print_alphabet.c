d5d4597e4aca593f08b7dc12e92cf5cba15f41ed
RichesUR7 <richesuche91@gmail.com>
2023-09-08 20:57:50 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the function of the task
+ * Return: 0 (Success)
+ */
+
+int main(void)
+
+{
+char i;
+
+for (i = 'a'; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}
