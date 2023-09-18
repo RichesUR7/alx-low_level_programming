79e47eb2cb2109849c67f700de15875b2fbfd698
RichesUR7 <richesuche91@gmail.com>
2023-09-18 12:03:32 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - function name
+ * @s: function parameter
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
