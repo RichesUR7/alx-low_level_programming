fa2e9c7966b3b42e9aa61bb1fd97fb5132cf18a9
RichesUR7 <richesuche91@gmail.com>
2023-09-18 16:29:45 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -13,5 +12,6 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+int _putchar(char c);
 
 #endif
