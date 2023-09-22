d519c6ddbeb1ca2b14730b009562db663a3de4d6
RichesUR7 <richesuche91@gmail.com>
2023-09-21 17:57:07 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i = 0, src_len = 0;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-{
-	while (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
-}
+	while (src[i++])
+	src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+	dest[i] = src[i];
+
+	for (i = src_len; i < n; i++)
+	dest[i] = '\0';
+
 	return (dest);
 }
