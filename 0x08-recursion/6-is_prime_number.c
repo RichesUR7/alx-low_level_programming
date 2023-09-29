99adf7a285ac728c7c46f27e669fb368b9d58b82
RichesUR7 <richesuche91@gmail.com>
2023-09-29 16:53:09 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 int actual_prime(int n, int i);
+
 /**
- * is_prime _number - says if an integer is a prime number or not
+ * is_prime_number - says if an integer is a prime number or not
  * @n: number to evaluate
  *
- * Return: 1 if n is a prime number, 0 is not
+ * Return: 1 if n is a prime number, 0 if not
  */
 
 int is_prime_number(int n)
@@ -15,13 +16,12 @@ int is_prime_number(int n)
 	return (actual_prime(n, n - 1));
 }
 
-
 /**
- * actual_prime - caiculates if a number is recursively
+ * actual_prime - calculates if a number is a prime recursively
  * @n: number to evaluate
- * @i; iterator
+ * @i: iterator
  *
- * Return: 1 if n is prime, 0 is not
+ * Return: 1 if n is prime, 0 if not
  */
 
 int actual_prime(int n, int i)
@@ -30,7 +30,5 @@ int actual_prime(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-
-
 	return (actual_prime(n, i - 1));
 }
