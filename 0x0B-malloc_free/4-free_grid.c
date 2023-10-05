df51993963f43dc0b70a07b1d14914dc55a53abf
RichesUR7 <richesuche91@gmail.com>
2023-10-05 15:20:56 -0700

    It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,17 @@
 #include "main.h"
+#include <stdlib.h>
+/**
+ * free_grid - function name
+ * @grid: parameter 1
+ * @height: parameter 2
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
