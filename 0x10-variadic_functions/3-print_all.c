3a60fc3ed7d6437a387f3a10abfdb88121e8264e
RichesUR7 <richesuche91@gmail.com>
2023-10-12 18:51:25 -0700

    To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,42 +7,44 @@
 */
 void print_all(const char * const format, ...)
 {
-	int i, check_stat; /* declare variables and va_arg datatype */
+	int i;
+	int flag;
 	char *str;
-	va_list spc;
+	va_list a_list;
 
-	va_start(spc, format); /* initialize var arguments */
+	va_start(a_list, format);
 	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
 		switch (format[i])
 		{
+			case 'c':
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
+				break;
 			case 'i':
-				printf("%d", va_arg(spc, int));
-				check_stat = 0; /* check if condition have been met */
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(spc, double));
-				check_stat = 0;
-				break;
-			case 'c':
-				printf("%c", va_arg(spc, int));
-				check_stat = 0;
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
 				break;
 			case 's':
-				str = va_arg(spc, char *);
+				str = va_arg(a_list, char*);
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
+				flag = 0;
 				break;
 			default:
-				check_stat = 1;
+				flag = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && check_stat == 0) /* if NOT NULL */
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
-		i++; /* update step of iter var */
+		i++;
 	}
 	printf("\n");
-	va_end(spc); /* end traversal */
+	va_end(a_list);
 }
