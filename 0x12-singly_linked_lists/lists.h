731b492a9da92a333b424b7e2c745d24986520ac
your_github_username <richesuche91@gmail.com>
2023-10-19 20:43:15 -0700

    List length

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,26 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly inked list
+ * @str: string (malloc'ed string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ * Description: singly linked list
+ */
+
+typedef struct list_s
+{
+	char *str;
+	size_t len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+#endif
