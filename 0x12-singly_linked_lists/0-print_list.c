f02c71257da9bfda3f0d8993fb299cb46d854b18
your_github_username <richesuche91@gmail.com>
2023-10-20 20:27:17 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
- * print_list - function name.
- * @h: pointer to the list_t list
- * Return: The number of nodes printed
+ * print_list - print all the element of a linked list
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes linked
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	int i;
 
-
-	while (h)
+	if (h == NULL)
+		return (0);
+	for (i = 1; h->next != NULL; i++)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-
 		h = h->next;
-
-		s++;
 	}
-
-
-	return (s);
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
 }
