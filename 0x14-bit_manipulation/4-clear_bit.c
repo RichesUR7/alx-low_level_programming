644aff1e6b51a854a13f301c00be3fee916e8d9e
your_github_username <richesuche91@gmail.com>
2023-10-26 17:13:59 -0700

    100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * clear_bit - function name
+ * @n: pointer of an unsigned long int
+ * @index: index of the bit
+ * Return: 1 if it works, -1 if it fails
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
+}
