751554b33d742b6c6b1ec492d619aa1447bc3a61
your_github_username <richesuche91@gmail.com>
2023-10-30 18:45:44 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,7 +16,7 @@ void error_file(int file_from, int file_to, char *argv[])
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read to %s\n", argv[2]);
 		exit(99);
 	}
 }
