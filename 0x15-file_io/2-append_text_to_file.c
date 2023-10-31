a7f97a67d397b36a53f99dde7536b3567ea0c22f
your_github_username <richesuche91@gmail.com>
2023-10-30 17:17:53 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		for (nletters = 0; text_context[nletters]; nletters++)
+		for (nletters = 0; text_content[nletters]; nletters++)
 			;
 
 		rwr = write(fd, text_content, nletters);
