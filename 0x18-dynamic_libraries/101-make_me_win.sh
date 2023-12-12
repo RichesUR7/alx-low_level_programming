bae69452dd41989d4d3a1f8696e36f12950cda38
your_github_username <richesuche91@gmail.com>
2023-12-12 15:09:10 -0800

    Created libdynamic.so file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/RichesUR7/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
--- 0x18-dynamic_libraries/functions/2-strncpy.c
+++ 0x18-dynamic_libraries/functions/2-strncpy.c
@@ -10,16 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index;
+	int i = 0, src_len = 0;
 
-	for (index = 0; index < n && src[index] != '\0'; index++)
-	{
-		dest[index] = src[index];
-	}
-	while (index < n)
-	{
-		dest[index] = '\0';
-		index++;
-	}
+	while (src[i++])
+	src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
